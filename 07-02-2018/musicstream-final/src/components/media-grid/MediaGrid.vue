5a652e0aa2efce6cdc18cfb1a19849514d8e6d0c
Lennie <lenny0307@gmail.com>
2018-07-01 15:32:05 -0400

    Added MediaItem component.

--- 07-02-2018/musicstream-final/src/components/media-grid/MediaGrid.vue
+++ 07-02-2018/musicstream-final/src/components/media-grid/MediaGrid.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>Videos go here.</div>
+    <media-item v-if="mediaItem" :video-url="mediaItem.uri" :video-title="mediaItem.name"></media-item>
 </template>
 
 <script>
+import MediaItem from '../media-item/MediaItem';
 import EventBus from '../../main';
 import vimeoService from '../../services/vimeo.service';
 
 export default {
+  data() {
+    return {
+      mediaItem: {},
+    };
+  },
+  components: {
+    MediaItem
+  },
   mounted() {
     EventBus.$on('search', (query) => {
       this.search(query);
@@ -15,6 +24,7 @@ export default {
   methods: {
     search(query) {
       vimeoService.searchVimeo(query).then((response) => {
+        this.mediaItem = response.data.data[0];
         console.log(response);
       }, (err) => {
         console.log(err);
---
+++ 07-02-2018/musicstream-final/src/components/media-item/MediaItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <b-card
+            :title="videoTitle">
+            <b-card-body>
+                {{videoUrl}}
+            </b-card-body>
+        </b-card>
+    </div>
+</template>
+
+<script>
+/*
+    What props does this component need? 
+    videoURL and VideoTitle
+*/
+export default {    
+    props: {
+        videoUrl: String,
+        videoTitle: String,
+    },
+};
+</script>
